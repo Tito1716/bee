8d96e894767625c1372d92b159b1317ecc4a0247
Titotono00@hotmail.es <Titotono00@hotmail.es>
2021-02-24 18:39:19 -0600

    Ac

--- app/views/public/templates/page.class.php
+++ app/views/public/templates/page.class.php
@@ -5,12 +5,14 @@ class Page extends Component{
         print("
         <!DOCTYPE html>
         <html lang='es'>
-        <head> 
+        <head>
             <meta charset='utf-8'>
             <title>$title</title>
                 <link type='text/css' rel='stylesheet' media='screen,projection' href='web/css/materialize.min.css'/>
                 <link type='text/css' rel='stylesheet' href='web/css/material_icons.css'/>
                 <link type='text/css' rel='stylesheet' href='web/css/main.css'/>
+                <link href='web/css/lightbox.css' rel='stylesheet' />
+                <link rel='stylesheet' href='web/css/venobox.css' type='text/css' media='screen'/>
                 <link rel='preconnect' href='https://fonts.gstatic.com'>
                 <link href='https://fonts.googleapis.com/css2?family=Jomhuria&display=swap' rel='stylesheet'>
                 <link href='https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,500&display=swap' rel='stylesheet'>
@@ -42,6 +44,8 @@ class Page extends Component{
         </footer>
             <!--se llaman las opciones para que la pagina pueda utlizar debidas especificaciones-->
             <script type='text/javascript' src='web/js/jquery-3.3.1.min.js'></script>
+            <script type='text/javascript' src='web/js/venobox.min.js'></script>> 
+            <script src='web/js/lightbox.js'></script>
             <script type='text/javascript' src='web/js/materialize.min.js'></script>
             <script type='text/javascript' src='web/js/main.js'></script>
          </body>
--- web/js/main.js
+++ web/js/main.js
@@ -12,26 +12,12 @@ $(document).ready(function(){
         padding:20,
         numVisible: 8
   });
-  $('.materialboxed').materialbox();
   $('.slider').slider();
+  $('.venobox').venobox();
 });
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true,
+  'disableScrolling' : true,
+  'fitImagesInViewport': true
+})
